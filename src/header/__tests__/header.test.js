3608f397379466b106c0e6989014ecf233b854fb
JP Richardson <jprichardson@gmail.com>
2016-05-26 17:19:59 -0500

    package: added flow types

---
+++ src/header/__tests__/header.test.js
@@ -0,0 +1,7 @@
+import test from 'tape'
+
+test('fuck', (t) => {
+  t.true(true)
+
+  t.end()
+})
---
+++ src/header/index.js
@@ -0,0 +1,25 @@
+/* @flow */
+import varstruct from 'varstruct'
+
+export const MAGIC = Buffer.from('SECO', 'utf8')
+
+export function checkMagic (magic) {
+  if (!magic.equals(MAGIC)) throw new RangeError('Invalid secure container magic.')
+}
+
+export const header = varstruct([
+  { name: 'magic', type: varstruct.Bound(varstruct.Buffer(4), checkMagic) },
+  { name: 'versionTag', type: varstruct.VarString(varstruct.UInt8) },
+  { name: 'appName', type: varstruct.VarString(varstruct.UInt8, 'utf-8') },
+  { name: 'appVersion', type: varstruct.VarString(varstruct.UInt8, 'utf-8') },
+  { name: 'metadata', type: varstruct.VarBuffer(varstruct.UInt32BE) },
+  { name: 'blob', type: varstruct.VarBuffer(varstruct.UInt32BE) }
+])
+
+export function decode (containerBlob: Buffer): Object {
+
+}
+
+export function encode (object): Buffer {
+
+}
